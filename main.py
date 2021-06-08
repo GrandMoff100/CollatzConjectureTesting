b4f24d7c5bcb15b2e3896bf4ea4c387bf64726bb
Nate <Nlarsen23.student@gmail.com>
2021-06-08 20:37:32 +0000

    Fixed rule implementation and added cache

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+STEP_CACHE = {}
+
+
 def collatz_rule(n: int) -> int:
-    if n % 2:
-        return n // 2
-    else:
-        return 3 * n + 1
+    return STEP_CACHE.get(n, n * 3 + 1 if n % 2 else n // 2)
 
 def collatz_steps(n :int) -> int:
-    print(n)
     if n == 1:
         return 0
     else:
         return 1 + collatz_steps(collatz_rule(n))
 
-i = 4
-print(i, collatz_steps(i))
+i = 1
+while True:
+    print(i, collatz_steps(i))
+    i += 1
